0d5cecff1c13e7e4276368ac59ed6dc6fee393cd
Dancer2009 <daniloMoura26@hotmail.com>
2022-09-22 13:39:52 -0300

    Add files via upload

---
+++ AzureMFA.cs
@@ -0,0 +1,24 @@
+namespace WebApplication1
+{
+    public class AzureMFA
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+    public class mfadto
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public string scope { get; set; }
+        public string session_state { get; set; }
+        public string state { get; set; }
+
+    }
+
+}
---
+++ SendMessage.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace WebApplication1
+{
+    public class SendMessage
+    {
+        public void ReturnResponse(string identificador)
+        {
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Key bGlhYXNzaXN0ZW50ZWNzZTpYR1RSYTduejNOOUgxOVVCejc4Rg==");
+            var objeto = new
+            {
+                id = "0001",
+                to = identificador,
+                type = "application/json",
+                content = new
+                {
+                    type = "template",
+                    template = new
+                    {
+                        name = "validar_mfa",
+                        language = new
+                        {
+                            code = "pt_BR",
+                            policy = "deterministic"
+                        }
+                    }
+                }
+            };
+            var content = ToRequest(objeto);
+            var response = httpClient.PostAsync(requestUri: "https://energisatap.http.msging.net/messages", content);
+           
+        }
+        private static StringContent ToRequest(object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj);
+            var data = new StringContent(json, Encoding.UTF8, mediaType: "application/json");
+            return data;
+        }
+    }
+}
